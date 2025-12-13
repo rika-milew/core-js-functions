4836a5e4b2e6b8472142c8aa9cc648f9df2be5f9
Erika Milevskaya <rika.gumerova@gmail.com>
2025-12-14 02:39:38 +0300

    feat: implement retry function

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -129,8 +129,18 @@ function memoize(/* func */) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return function call(...args) {
+    let error;
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func(...args);
+      } catch (currentError) {
+        error = currentError;
+      }
+    }
+    throw error;
+  };
 }
 
 /**
