9410dae91cc40007df83c64b535925239801362f
Erika Milevskaya <rika.gumerova@gmail.com>
2025-12-17 00:42:28 +0300

    feat: implement logger function

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -181,8 +181,15 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return function loggerFn(...args) {
+    const argumentsFn = args.map((arg) => JSON.stringify(arg)).join(',');
+    const functionName = func.name;
+    logFunc(`${functionName}(${argumentsFn}) starts`);
+    const log = func(...args);
+    logFunc(`${functionName}(${argumentsFn}) ends`);
+    return log;
+  };
 }
 
 /**
